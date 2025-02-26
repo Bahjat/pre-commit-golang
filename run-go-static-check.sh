38ba8e985e844a231269ce8af7d06288a7413208
Valentin Krasontovitsch <v.krasontov@gmail.com>
2025-02-26 09:02:31 +0100

    feat: accept args for staticcheck
    staticcheck needs some massaging of the input files given to it by
    pre-commit - while staticcheck can run on single files, it will complain
    if there are missing dependencies that get resolved in another file in
    the same package. Furthermore, if it is passed several files, it demands
    that those files live in the same package.
    
    hence the approach has been to parse the file arguments and transform
    them into their respective packages, calling staticcheck on one package
    at a time.
    
    the simple original solution did not allow for arguments to staticcheck
    to be passed via pre-commit's `args` option.
    
    we iterate on the original solution by parsing the passed arguments,
    separating possible command line options and file arguments.
    
    resolves #11

--- run-go-static-check.sh
+++ run-go-static-check.sh
@@ -1,5 +1,43 @@
 #!/bin/sh
 
-for DIR in $(echo "$@"|xargs -n1 dirname|sort -u); do
-    staticcheck ./"$DIR"
+set -euo pipefail
+
+cmd_options=()
+file_args=()
+
+# we parse out cli options and file arguments
+# technically we allow for options to be passed after the file arguments, but
+# we know that pre-commit puts the file arguments last
+# also, we do not handle file names starting with a hyphen
+while [[ $# -gt 0 ]]; do
+    if [[ "$1" == -* ]]; then
+        # If it's an option, ensure there's a value after it
+        if [[ $# -ge 2 && -n "$2" && "$2" != -* ]]; then
+            cmd_options+=("$1" "$2")
+            shift 2
+        else
+            echo "Error: Missing value for key $1"
+            exit 1
+        fi
+    else
+        file_args+=("$1")
+        shift
+    fi
+done
+
+if [[ ${#file_args[@]} -eq 0 ]]; then
+    echo "Error: No file arguments provided"
+    exit 1
+fi
+
+packages=()
+
+for DIR in $(echo "${file_args[@]}" | xargs -n1 dirname | sort -u); do
+    packages+=("$DIR")
+done
+
+[[ ${#cmd_options[@]} -eq 0 ]] && cmd_options+=("")
+
+for package in "${packages[@]}"; do
+    staticcheck ${cmd_options[@]} ./"${package}"
 done
